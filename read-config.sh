d730f1d49da08dd68ae9bd1983a89feef2ade6ea
Christopher Lovejoy <c@lovejoy.io>
2026-03-25 11:38:14 -0700

    read-config escape value more reliably, simply

--- read-config.sh
+++ read-config.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+READ_CONFIG_VERSION=0.6.5
+READ_CONFIG_MODIFIED=2026-03-25
+
 shopt -s extglob # For +() pattern matching (stripping indentation, mushing -vv together).
 read-config()
 {
@@ -60,7 +64,7 @@ read-config()
   local contextName= contextSuffix= isContextMatched=
 
   assignKey() {
-    local escapedValue isAppending assignmentOperator='='
+    local isAppending assignmentOperator='='
     while [[ $# -ge 3 ]]
     do
     case $1 in
@@ -75,13 +79,12 @@ read-config()
     esac
       shift
     done
-    local key="$1" value="$2"
-    escapedValue="$(echo -E "$value" | sed 's/\([\\"$]\)/\\\1/g')"
-    local assignmentRightSide="${assignmentOperator}\"${escapedValue}\""
-    eval "${configNameBase}${contextSuffix}[${key}]${assignmentRightSide}"
+    local escapedKey="$(printf %q "$1")"
+    local assignmentRightSide="${assignmentOperator}$(printf %q "$2")"
+    eval "${configNameBase}${contextSuffix}[${escapedKey}]${assignmentRightSide}"
     if [[ -n $contextSuffix && $isContextMatched == true ]]
     then
-      eval "${configNameBase}[${key}]${assignmentRightSide}"
+      eval "${configNameBase}[${escapedKey}]${assignmentRightSide}"
     fi
   }
 
@@ -192,6 +195,7 @@ help-read-config()
   echo "CONFIG FORMAT:"
   echo "  The config format is akin to TOML, but stupider."
   echo "  * Definitions are 'KEY=VALUE', without space around '=', and without quotation."
+  echo "  * VALUE beginning with '~' will have \$HOME substituted for '~'"
   echo "  * Lines in square brackets begin a CONTEXT."
   echo "  * Indentation is ignored everywhere."
   echo "  * Lines ending with '\\' backslashes carry over VALUE definitions into the next line."
@@ -225,5 +229,5 @@ help-read-config()
   echo
   echo "NOTE: Definitions won't reach the call context if read-config is in a subshell, such as \`echo key=value | read-config\`. You might do \`read-config <<<\"\$(echo key=value)\"\` instead."
   echo
-  echo "(read-config version 0.6.4 2026-03-14)"
+  echo "(read-config version ${READ_CONFIG_VERSION} ${READ_CONFIG_MODIFIED})"
 }
